6ed29c9970ef9812096879f13de2dfd90c6d9681
mohammad.faiz <faiz.akram90@hotmail.com>
2020-05-31 01:32:08 +0530

    Code

---
+++ Mathfunction.py
@@ -0,0 +1,17 @@
+"""import math
+print(math.sqrt(25))
+print(math.pow(2,2))
+print(math.floor(34.7))
+print(math.ceil(34.1))
+print(math.pi)
+print(math.e)"""
+
+"""import math as m
+print(m.sqrt(23))
+print(m.factorial(12))"""
+
+from math import sqrt,ceil,pi
+print(ceil(90.1))
+print(sqrt(90.1))
+print(pi)
+
